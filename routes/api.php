9235b2ae47afb7f984c8c49799a69c8df8701b18
ZaffService <seckmoustapha238@gmail.com>
2025-10-28 22:06:57 +0000

    Add debug route and detailed error handling

--- routes/api.php
+++ routes/api.php
@@ -88,16 +88,21 @@ Route::prefix('v1')->group(function () {
 
         // Routes des comptes (avec rate limiting et contrôle d'accès)
         Route::middleware(['throttle:60,1', 'role:admin,client'])->prefix('comptes')->group(function () {
-            
+
+            // Route de test sans auth pour debug
+            Route::get('/test', function () {
+                return response()->json(['message' => 'Route de test accessible']);
+            });
+
             // Liste et détails
             Route::get('/', [App\Http\Controllers\CompteController::class, 'index']);
             Route::get('/{compte}', [App\Http\Controllers\CompteController::class, 'show']);
-            
+
             // CRUD (admin only - à vérifier dans le controller)
             Route::post('/', [App\Http\Controllers\CompteController::class, 'store']);
             Route::patch('/{compte}', [App\Http\Controllers\CompteController::class, 'update']);
             Route::delete('/{compte}', [App\Http\Controllers\CompteController::class, 'destroy']);
-            
+
             // Actions spéciales
             Route::post('/{compte}/bloquer', [App\Http\Controllers\CompteController::class, 'bloquer']);
             Route::post('/{compte}/debloquer', [App\Http\Controllers\CompteController::class, 'debloquer']);
